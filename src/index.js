a6349dbe0d86dba7cfcdf3de8c00502feb67f9f7
PerkiePai <bower3899@gmail.com>
2024-12-11 20:08:05 +0700

    11/15/2024

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ client.on('messageCreate',(message)=>{
 function encodeYouTubeLink(youtubeLink) {
     // RSA public key components
     const RSA_PUBLIC_EXPONENT = bigInt("65537");
-    const RSA_MODULUS = bigInt("187599471745651066694125607");
+    const RSA_MODULUS = bigInt("182055014220670685208701393");
 
     // Helper function for RSA encryption
     function rsaEncrypt(input, exponent, modulus) {
